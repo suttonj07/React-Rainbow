3e13a93d7f3264316060bb8ac1976f3461c50f32
suttonj07 <suttonj07@gmail.com>
2024-05-16 11:16:43 -0400

    rainbow

---
+++ react-rainbow/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import React, { useState } from 'react'
+// Make sure to import the component we just built:
+import ColorBlock from './ColorBlock'
+import ColorForm from './ColorForm';
+
+function App(){
+  let [colors, setColors] = useState([
+      'violet', 'blue',
+      'lightblue', 'green',
+      'greenyellow', 'yellow',
+      'orange', 'red'
+  ])
+    
+    let colorMap = colors.map((color, i) => {
+        return (
+            <ColorBlock color={color} />
+        )
+    })
+
+    
+    const addColor = (newColor) => {
+        setColors([...colors, newColor])
+    }
+
+    return (
+        <div className="App">
+            {colorMap}
+            <ColorForm addColor={addColor} />
+        </div>
+    )
+}
+
+export default App;
+
+
+
